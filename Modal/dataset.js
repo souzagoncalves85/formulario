018c670d732e201cdbec29b2c038c256c3744fdb
souzagoncalves85 <110495352+souzagoncalves85@users.noreply.github.com>
2022-08-17 22:17:30 -0300

    Add files via upload

---
+++ Modal/dataset.js
@@ -0,0 +1,19 @@
+const itens=[
+    {
+        id:1,
+        name:'bolo',
+        image:"../imagem/bolo (1).jpg"
+    },
+    {
+        id:2,
+        name:'café',
+        image:"../imagem/cafe.jpg"
+    },
+
+    {
+        id:3,
+        name:'cookie',
+        image:"../imagem/cookie.jpg"
+    }
+]
+export default itens
